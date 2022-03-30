1fe370b61e210805e42896b3802e9f71072a63ac
Lisa Knox <lisaknox83@gmail.com>
2022-03-30 09:04:32 -0400

    start cleaning up this mess lol

---
+++ readme.md
@@ -0,0 +1,6 @@
+5 words vert, 5 words horiz, middle square revealed by default (can toggle for
+extra challenge) words "rotate" vert and horiz to get to disred word block when
+a 5 letter word that is in the puzzle is made, highlighted?
+
+- or just a 5 letter word on the list? - I think this one, don't want it to be
+  too easy
---
+++ squares.js
@@ -0,0 +1,12 @@
+const parsedList = require('./workup');
+// console.log(parsedList);
+
+/*
+pick 5 words for rows
+see if one letter from each rows can spell a word
+see if the rest of the letters can spell another word, down to last letters
+*/
+
+/*
+instead of guess and check start with target word and see how many ways it can be built with one letter for 5 different other words
+*/
