19d91c11ccdfe405e882ba329c18405740f6ab01
Lisa Knox <lisaknox83@gmail.com>
2022-04-05 09:21:20 -0400

    relocated data, started treeification

--- src/body.js
+++ src/body.js
@@ -3,12 +3,12 @@ import testDisplay from '../db/index';
 
 export default class Body extends React.Component {
   render() {
-    console.log(testDisplay);
+    // console.log(testDisplay);
     return (
       <div className="container-flush">
         <ul>
-          {testDisplay.map((x) => (
-            <li>{x}</li>
+          {testDisplay.map((x, i) => (
+            <li key={i}>{x}</li>
           ))}
         </ul>
       </div>
